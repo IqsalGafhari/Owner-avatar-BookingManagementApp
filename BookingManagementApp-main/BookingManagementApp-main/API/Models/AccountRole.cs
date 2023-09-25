64f1ad8b0b2fd63a4006d9acf2cdc3130a0532e8
J3C218193 <72546948+J3C218193@users.noreply.github.com>
2023-09-25 15:23:54 +0700

    AccountRole.cs

--- BookingManagementApp-main/BookingManagementApp-main/API/Models/AccountRole.cs
+++ BookingManagementApp-main/BookingManagementApp-main/API/Models/AccountRole.cs
@@ -1,7 +1,4 @@
-﻿using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-
-namespace BookingManagementApp.Controllers
+﻿namespace API.Models;
 {
     public class AccountRole 
     {
